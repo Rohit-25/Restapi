1e5e2e72183e745afd3d948413fd706d71f8b6c1
rohit <rohitgadekar6@gmail.com>
2022-12-16 13:22:17 +0530

    first commit

---
+++ apidoc.md
@@ -0,0 +1,42 @@
+*First Page*
+# Top Strip
+http://localhost:2500/topstrip
+# Shop by category
+http://localhost:2500/category
+# Shop by Gender
+http://localhost:2500/gender
+
+
+*Second Page*
+# List of Jwellery wrt category
+http://localhost:2500/shop/jewllery?category_id=1
+
+# List of Jwellery wrt Gender
+http://localhost:2500/shop/gender?gender_id=1
+
+# Sort Jwellery category on basis of price
+http://localhost:2500/filter/1?lcost=100&hcost=95000&sort=-1
+
+# sort gender based jwell on basis of price
+http://localhost:2500/filtergender/1?lcost=100&hcost=95000&sort=-1
+
+
+
+*Third Page*
+# Detail of the jwellery
+http://localhost:2500/proddetail/rings?item_id=1
+http://localhost:2500/proddetail/earrings?item_id=1
+http://localhost:2500/proddetail/coins?item_id=1
+http://localhost:2500/proddetail/pendents?item_id=1
+
+# Place Order 
+
+*Fourth Page*
+# List of Order
+
+# Delete Order
+
+# List of order wrt to email
+
+*Fifth Page*
+# 
---
+++ app.js
@@ -0,0 +1,214 @@
+let express = require(`express`);
+let app = express();
+let env = require(`dotenv`);
+env.config();
+let port=process.env.PORT || 9800;
+
+
+let cors = require(`cors`);
+let mongo = require(`mongodb`);
+let  mongoClient = mongo.MongoClient;
+let mongoUrl = process.env.MongoLive;
+let bodyParser = require('body-parser');
+let db;
+
+//middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get('/',(req,res) => {
+    res.send('This is From Express App code')
+})
+
+app.get('/topstrip',(req,res) => {
+    db.collection('topstrip').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/category',(req,res) => {
+    db.collection('category').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/gender',(req,res) => {
+    db.collection('gender').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/shop/jewllery',(req,res) => {
+    let query={};
+    let category_id =Number(req.query.category_id) ;
+    if(category_id ){
+        query={"category_id":category_id }
+    }
+    db.collection('jwellList').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/shop/gender',(req,res) => {
+    let query={};
+    let gender_id =Number(req.query.gender_id) ;
+    if(gender_id ){
+        query={"gender_id":gender_id }
+    }
+    db.collection('jwellList').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/filtercategory/:category_id',(req,res) => {
+    let query = {};
+    let sort = {"new_price":1}
+    let category_id= Number(req.params.category_id);
+    query={"category_id":category_id};
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost);
+    if(req.query.sort){
+        sort={"new_price":req.query.sort}
+    }
+    if(lcost && hcost){
+        query={
+            "category_id":category_id,
+            
+            $and:[{new_price:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+ 
+    
+    db.collection('jwellList').find(query).sort(sort).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.get('/filtergender/:gender_id',(req,res) => {
+    let query = {};
+    let sort = {"new_price":1}
+    let gender_id= Number(req.params.gender_id);
+    query={"gender_id":gender_id};
+    let lcost = Number(req.query.lcost);
+    let hcost = Number(req.query.hcost);
+    if(req.query.sort){
+        sort={"new_price":req.query.sort}
+    }
+    if(lcost && hcost){
+        query={
+            "gender_id":gender_id,
+            
+            $and:[{new_price:{$gt:lcost,$lt:hcost}}]
+        }
+    }
+ 
+    
+    db.collection('jwellList').find(query).sort(sort).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//page 3
+app.get('/proddetail/rings',(req,res) => {
+    let query={};
+    let item_id =Number(req.query.item_id) ;
+    if(item_id ){
+        query={"item_id":item_id }
+    }
+    db.collection('rings').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+app.get('/proddetail/coins',(req,res) => {
+    let query={};
+    let item_id =Number(req.query.item_id) ;
+    if(item_id ){
+        query={"item_id":item_id }
+    }
+    db.collection('coins').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+app.get('/proddetail/earrings',(req,res) => {
+    let query={};
+    let item_id =Number(req.query.item_id) ;
+    if(item_id ){
+        query={"item_id":item_id }
+    }
+    db.collection('earrings').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+app.get('/proddetail/pendents',(req,res) => {
+    let query={};
+    let item_id =Number(req.query.item_id) ;
+    if(item_id ){
+        query={"item_id":item_id }
+    }
+    db.collection('pendents').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+//add to cart
+app.post('/cart',(req,res) => {
+    console.log(req.body);
+    db.collection('cart').insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.send('added to cart')
+    })
+})
+
+// show orders
+
+app.get('/orders',(req,res) => {
+    db.collection('cart').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+app.delete('/deleteOrder/:id',(req,res) => {
+    let item_id= Number(req.params.id);
+    // let item_id = mongo.ObjectId(req.params.id);
+    db.collection('cart').deleteOne({item_id},(err,result) => {
+        if(err) throw err;
+        res.send('Order Deleted')
+    })
+})
+
+//5thpAGE
+//orderplaced
+//email, ordersummery, delivery details
+app.post('/orderplaced',(req,res) => {
+    console.log(req.body);
+    db.collection('orders').insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.send('added to cart')
+    })
+})
+
+
+
+
+
+//connection with mongo
+mongoClient.connect(mongoUrl,(err,client)=>{
+    if(err) console.log(`Error while connecting`);
+    db = client.db('Tanishqdb')
+    app.listen(port,() => {
+        console.log(`Listing to port ${port}`)
+    })
+})
