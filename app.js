59a94ca360c01fd15222215c4da2bec95f47569c
rohit <rohitgadekar6@gmail.com>
2022-12-29 19:30:36 +0530

    changes

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Origin');
     next();
   });
 
