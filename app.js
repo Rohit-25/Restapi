dd01b21024905be00990571e5c8882d5ecf82690
rohit <rohitgadekar6@gmail.com>
2022-12-29 19:50:50 +0530

    changes

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Headers', 'Origin');
+    res.header('Access-Control-Allow-Headers', 'Accept');
+    res.header('Access-Control-Allow-Methods', 'Post');
+    res.header('Access-Control-Allow-Methods', 'Options');
     next();
   });
 
