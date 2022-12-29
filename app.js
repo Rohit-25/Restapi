f850074bb6eaf2eb71c4b41ac8ae1b63f8ad76fa
rohit <rohitgadekar6@gmail.com>
2022-12-29 17:25:05 +0530

    changes

--- app.js
+++ app.js
@@ -13,7 +13,13 @@ let bodyParser = require('body-parser');
 let db;
 
 //middleware
-// app.use(cors());
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 app.use((req, res, next) => {
